9d62cc587f85795dd75029a2fccfb9865952d228
Paolo Insogna <paolo@cowtech.it>
2025-09-04 13:30:24 +0200

    fixup
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,37 @@
+import { BaseCapability } from '@platformatic/basic'
+import { BaseGenerator } from '@platformatic/generators'
+import {
+  ApplicationCapability,
+  ServerInstance as ApplicationInstance,
+  ConfigurationOptions
+} from '@platformatic/service'
+import { JSONSchemaType } from 'ajv'
 import { FastifyInstance } from 'fastify'
-import { PlatformaticApp } from '@platformatic/service'
-import { PlatformaticPgHooksConfig } from './config'
+import { PlatformaticPgHooksConfiguration } from './config'
 
-declare module 'fastify' {
-  interface FastifyInstance {
-    platformatic: PlatformaticApp<PlatformaticPgHooksConfig>
-  }
+export { PlatformaticService } from '@platformatic/service'
+export { PlatformaticPgHooksConfiguration } from './config'
+
+export type PgHooksCapability = ApplicationCapability<PlatformaticPgHooksConfiguration>
+
+export type ServerInstance = ApplicationInstance<PlatformaticPgHooksConfiguration>
+
+export function create (
+  root: string,
+  source?: string | PlatformaticPgHooksConfiguration,
+  context?: ConfigurationOptions
+): Promise<PgHooksCapability>
+
+export declare function pgHooks (app: FastifyInstance, capability: BaseCapability): Promise<void>
+
+export class Generator extends BaseGenerator.BaseGenerator {}
+
+export declare const packageJson: Record<string, unknown>
+
+export declare const schema: JSONSchemaType<PlatformaticPgHooksConfiguration>
+
+export declare const schemaComponents: {
+  hooks: JSONSchemaType<object>
 }
 
-export { PlatformaticApp, PlatformaticPgHooksConfig }
+export declare const version: string
